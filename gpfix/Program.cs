b05e5f6311aa39c2aa8219d89ca05997c28300c7
Hari Lubovac <hari.lubovac@carvana.com>
2020-06-09 23:51:58 -0700

    CLI utility producing some info on network-adapter
    ... which can be used to re-prioritize routes after they are [commonly] rewritten by specialized VPN clients - e.g. making it possible to still access LAN resources. Example script provided (Windows only).

---
+++ gpfix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Linq;
+
+namespace gpfix
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return 1;
+            }
+
+            string adapterName = args[0];
+            string command = args[1];
+
+
+            var adapters = NetworkInterface.GetAllNetworkInterfaces();
+            var adapter = adapters.FirstOrDefault(i => i.Description == adapterName);
+
+            if (adapter == null)
+                return 2;
+
+            var properties = adapter.GetIPProperties();
+
+            string result = null;
+            if (command == "ip")
+                result = properties.UnicastAddresses.FirstOrDefault()?.Address.ToString();
+            else if (command == "index")
+                result = properties.GetIPv4Properties().Index.ToString();
+            else
+                return 3;
+
+            Console.WriteLine(result);
+
+            return 0;
+
+        }
+    }
+}
